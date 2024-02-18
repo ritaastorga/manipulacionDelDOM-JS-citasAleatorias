5e79aa1e183c11133bbbe3ece37ace318cce8431
rita <ritaastorga10@gmail.com>
2024-02-17 22:43:47 -0300

    realice la primera parte de la lógica del proyecto, en donde se puede visualizar la cita en la página

---
+++ app.js
@@ -0,0 +1,15 @@
+let botonElemento = document.getElementById("botonCambiarCita");
+let citaElemento = document.getElementById("cita");
+let autorElemento = document.getElementById("autor");
+
+function generarEnteroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+  citaElemento.innerText = `'${citas[indiceAleatorio].texto}'`;
+  autorElemento.innerText = citas[indiceAleatorio].autor;
+}
+
+cambiarCita();
